4fddaefbddcf6262ce6f9bf1dced5ff38eb3f132
vllik <94060164+vllik@users.noreply.github.com>
2024-09-14 22:13:07 +0300

    Add files via upload

---
+++ exercise1/scripts.js
@@ -0,0 +1,6 @@
+function displayQuote() {
+    let quote = "<p><em>\"If I had nine hours to chop down a tree, I'd spend the first six sharpening my ax.\"</em></p>" +
+                "<p><strong>-- Abraham Lincoln</strong></p>";
+
+    document.body.innerHTML = quote;
+}
