b15b47b7ad4dcbf5b8574940c73f97e18ec83786
vllik <94060164+vllik@users.noreply.github.com>
2024-09-14 22:30:54 +0300

    Add files via upload

---
+++ exercise5/farm.js
@@ -0,0 +1,5 @@
+var images =[
+    "http://farm4.staticflickr.com/3691/11268502654_f28f05966c_m.jpg",
+    "http://farm1.staticflickr.com/33/45336904_1aef569b30_n.jpg",
+    "http://farm6.staticflickr.com/5211/5384592886_80a512e2c9.jpg"
+    ];
